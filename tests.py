abcb063838abd0bcad384eb46ff802daddb43a00
Latinoware 2010 <ramiroluz@gmail.com>
2010-11-11 12:19:36 -0200

    primera dupla dojo passou no teste 1

--- tests.py
+++ tests.py
@@ -1,9 +1,22 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
 
+Telefone=499021
+
 def Tem6Digitos(NTelefone):
     return len(str(NTelefone))==6
 
-def test1():
-    assert Tem6Digitos(499021)==True
+def PrimeiroUltimoIguais(NTelefone):
+    stelefone = str(NTelefone)
+    return stelefone[0] == stelefone[-1]
 
+def test1():
+    assert Tem6Digitos(Telefone)==True
+    
+def test2():
+    #testa se o primeiro e ultimo digito sao iguais
+    assert PrimeiroUltimoIguais(Telefone)==False
+
+def test3():
+    #testa se ha numero consecutivo iguais
+    assert NumerosConsecutivo(Telefone)==True
